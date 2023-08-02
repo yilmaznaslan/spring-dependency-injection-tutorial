598daf460fabe4683252e1434d2913a687efcff6
Yilmaz Naci Aslan <yilmaznaci.aslan@maibornwolff.de>
2023-08-02 17:23:19 +0200

    remove challenge code

--- src/main/java/com/yilmaznaslan/ecommerce/order/OrderService.java
+++ src/main/java/com/yilmaznaslan/ecommerce/order/OrderService.java
@@ -1,24 +1,16 @@
 package com.yilmaznaslan.ecommerce.order;
 
-import com.yilmaznaslan.ecommerce.campaign.CampaignService;
 import com.yilmaznaslan.ecommerce.customer.CustomerService;
-import com.yilmaznaslan.ecommerce.inventory.InventoryService;
-import com.yilmaznaslan.ecommerce.notification.NotificationService;
 import com.yilmaznaslan.ecommerce.payment.PaymentService;
 import com.yilmaznaslan.ecommerce.product.ProductService;
-import com.yilmaznaslan.ecommerce.shipping.ShippingService;
 
 import java.util.List;
 
 public class OrderService {
-    private OrderRepository orderRepository;
-    private CustomerService customerService;
-    private ProductService productService;
-    private PaymentService paymentService;
-    private InventoryService inventoryService;
-    private ShippingService shippingService;
-    private NotificationService notificationService;
-    private CampaignService campaignService;
+    private final OrderRepository orderRepository;
+    private final CustomerService customerService;
+    private final ProductService productService;
+    private final PaymentService paymentService;
 
     public OrderService(OrderRepository orderRepository, CustomerService customerService, ProductService productService,
                         PaymentService paymentService) {
